b3ffa6fbb94f3f3ffc4b7b52411610b1bb983330
Will Ockmore <will.ockmore@gmail.com>
2017-12-03 22:09:25 +0000

    Program displays transactions and categorises
    Terminal loop shows the upcoming transactions and current.
    Keys to categorise are displayed and once complete a csv is written
    which can be uploaded to the google sheet.

--- read_statement.py
+++ read_statement.py
@@ -1,13 +1,31 @@
+import os
+import sys
 import csv
 import argparse
+
 import pandas as pd
-import json
-import sys
+from termcolor import colored
 
 INSTITUTIONS = [
     'amex',
     'halifax'
 ]
+
+CATEGORIES_KEYMAP = {
+    'q': 'Groceries',
+    'w': 'Rent',
+    'e': 'Bills',
+    'a': 'Travel',
+    's': 'Material Items',
+    'd': 'Entertainment',
+    'z': 'Savings',
+    'x': 'Eating Out/Drinks',
+    'v': 'Earnings',
+    't': 'Transfers',
+    'f': 'Cash Withdrawal',
+    'c': 'Miscellaneous',
+    'r': 'Not Applicable'
+}
 
 
 def get_halifax_record(row):
@@ -47,6 +65,70 @@ def read_csv(csv_file, institution):
     ]
 
 
+def print_record(record, suffix=''):
+    # format a record to display in the terminal
+
+    result = '{date}  {reference:50}{debit:7} {credit:7}{suffix}'.format(
+        suffix=suffix,
+        date=record['date'],
+        reference=colored(
+            record['reference'],
+            'yellow'
+        ),
+        debit=colored(
+            record['debit'] if record['debit'] else '',
+            'red'
+        ),
+        credit=colored(
+            record['credit'] if record['credit'] else '',
+            'green'
+        ),
+    )
+    return print(result)
+
+
+def categorise_record(record, records):
+    '''
+    Clear the terminal and display the record with
+    options for categorisation.
+    '''
+    os.system('clear')
+    print()
+    print(colored(
+        'Transactions:',
+        attrs=['bold', 'underline']
+    ))
+    print()
+
+    index = records.index(record)
+
+    print_record(record, suffix=colored('   <-', 'green'))
+
+    for _record in records[index + 1:index + 15]:
+        print_record(_record)
+
+    print()
+    print(colored('Categories:', attrs=['bold']))
+    print()
+
+    for key, category in CATEGORIES_KEYMAP.items():
+        print('{:20} {}'.format(category + ':', key))
+    print()
+
+    category_choice = input(
+        colored('Choose and press enter:', 'cyan')
+    )
+
+    while category_choice not in CATEGORIES_KEYMAP:
+        category_choice = input(
+            colored('Unknown category, try again:', 'red')
+        )
+
+    record['category'] = CATEGORIES_KEYMAP[category_choice]
+
+    return record
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
@@ -60,6 +142,16 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     records = read_csv(args.infile, args.source)
+    dates = [record['date'] for record in records]
 
-    for record in records:
-        pass
+    records_with_categories = [
+        categorise_record(record, records)
+        for record
+        in records
+    ]
+
+    pd.DataFrame(records_with_categories).to_csv(
+        '{}-{}-{}.csv'.format(dates[0].replace('/', ''), dates[-1].replace('/', ''), args.source),
+        index=False,
+        header=False
+    )
